cfb093f58d8eb74c7019cbb2f0424678a54d21e0
JoAn <joannacalinska@gmail.com>
2018-06-08 08:19:00 +0200

    update

---
+++ public/src/actions/add_person.js
@@ -0,0 +1,14 @@
+import { ADD_PERSON } from './types';
+
+export default function addPerson(person) {
+  return dispatch => {
+    dispatch(addPersonAsync(person));
+  }
+}
+
+function addPersonAsync(person) {
+  return {
+    type: ADD_PERSON,
+    payload: person,
+  }
+}
